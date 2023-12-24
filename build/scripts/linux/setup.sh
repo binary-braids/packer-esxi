83b1b068b44c615f7530f1a9b3d8dbf7b05e777b
data-tangles <cuanleo@gmail.com>
2023-12-24 12:03:54 +0200

    modify cleanup script

--- build/scripts/linux/setup.sh
+++ build/scripts/linux/setup.sh
@@ -31,17 +31,7 @@ rm /var/lib/dbus/machine-id
 ln -s /etc/machine-id /var/lib/dbus/machine-id
 
 # Clean cloud-init data
-# cloud init: move files and change permissions
-mv /tmp/cloud-init/* /etc/cloud/cloud.cfg.d/
-chown -R root:root /etc/cloud/cloud.cfg.d/
-
-# cleanup cloud-init data
+echo '> Cleaning cloud-init data ...'
 rm -rf /var/lib/cloud/*
-
 ln -s /var/lib/cloud/instances /var/lib/cloud/instance
-
-# cleanup cloud-init logs
 rm -rf /var/log/cloud-init*
-
-# cleanup tmp files
-rm -rf /tmp/cloud-init
